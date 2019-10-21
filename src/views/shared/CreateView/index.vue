db1062f335f5724d05903c4979442fb5b9c580ca
fengwenqiang <fengwenqiang>
2019-10-21 14:56:38 +0800

    创建创建视图组件

---
+++ src/views/shared/CreateView/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>创建视图</div>
+</template>
+
+<script>
+import handleTable from '@src/mixins/handleTable'
+import handleBreadcrumb from '@src/mixins/handleBreadcrumb.js'
+export default {
+  name: 'CreateView',
+  mixins: [handleTable, handleBreadcrumb],
+  mounted () {
+    this.pushBreadcrumb({
+      name: '创建视图',
+      parent: {
+        name: 'ViewManagement',
+        query: {
+          type: 'back'
+        }
+      }
+    })
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
