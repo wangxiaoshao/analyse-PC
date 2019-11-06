0e779d06cf1a2e0da4fafdd11d0785afb96c7ec2
Callous2 <zjpp.007@qq.com>
2019-11-06 17:47:57 +0800

    2019/11/06 - 路由拆分

---
+++ src/router/modules/examine.js
@@ -0,0 +1,87 @@
+
+const WaitApproval = () => import('@src/views/wait-approval/index.vue')
+const WaitApprovalList = () => import('@src/views/wait-approval/WaitApprovalList/index.vue')
+const WaitApprovalDetail = () => import('@src/views/wait-approval/WaitApprovalDetail/index.vue')
+const Approved = () => import('@src/views/approved/index.vue')
+const ApprovedList = () => import('@src/views/approved/ApprovedList/index.vue')
+const ApprovedDetail = () => import('@src/views/approved/ApprovedDetail/index.vue')
+const MyApplication = () => import('@src/views/my-application/index.vue')
+const MyApplicationList = () => import('@src/views/my-application/MyApplicationList/index.vue')
+const MyApplicationDetail = () => import('@src/views/my-application/MyApplicationDetail/index.vue')
+const ConfirmInfo = () => import('@src/views/confirm-info/index.vue')
+const ConfirmInfoList = () => import('@src/views/confirm-info/ConfirmInfoList/index.vue')
+const ConfirmInfoDetail = () => import('@src/views/confirm-info/ConfirmInfoDetail/index.vue')
+
+
+export default [
+  { // 待我审核
+    path: '/wait-approval',
+    name: 'WaitApproval',
+    component: WaitApproval,
+    children: [
+      {
+        path: '/',
+        name: 'WaitApprovalList',
+        component: WaitApprovalList
+      },
+      {
+        path: 'detail',
+        name: 'WaitApprovalDetail',
+        component: WaitApprovalDetail
+      }
+    ]
+
+  },
+  { // 我已审核
+    path: '/approved',
+    name: 'Approved',
+    component: Approved,
+    children: [
+      {
+        path: '/',
+        name: 'ApprovedList',
+        component: ApprovedList
+      },
+      {
+        path: 'detail',
+        name: 'ApprovedDetail',
+        component: ApprovedDetail
+      }
+    ]
+  },
+  { // 我的申请
+    path: '/my-application',
+    name: 'MyApplication',
+    component: MyApplication,
+    children: [
+      {
+        path: '/',
+        name: 'MyApplicationList',
+        component: MyApplicationList
+      },
+      {
+        path: 'detail',
+        name: 'MyApplicationDetail',
+        component: MyApplicationDetail
+      }
+    ]
+  },
+  { // 信息确认
+    path: '/confirm-info',
+    name: 'ConfirmInfo',
+    component: ConfirmInfo,
+    children: [
+      {
+        path: '/',
+        name: 'ConfirmInfoList',
+        component: ConfirmInfoList
+      },
+      {
+        path: 'detail',
+        name: 'ConfirmInfoDetail',
+        component: ConfirmInfoDetail
+      }
+    ]
+  },
+]
+
---
+++ src/router/modules/share.js
@@ -0,0 +1,57 @@
+
+const Shared = () => import('@src/views/shared/index.vue')
+const ViewManagement = () => import('@src/views/shared/view-management/index.vue')
+const CreateView = () => import('@src/views/shared/create-view/index.vue')
+const GroupManagement = () => import('@src/views/shared/group-management/index.vue')
+const GroupDetail = () => import('@src/views/shared/group-detail/index.vue')
+const AppManagement = () => import('@src/views/shared/app-management/index.vue')
+const CreateAppManagement = () => import('@src/views/shared/create-app-management/index.vue')
+const TagsManagement = () => import('@src/views/shared/tags-management/index.vue')
+
+
+export default [
+  // 共享管理
+  {
+    path: '/shared',
+    name: 'Shared',
+    component: Shared,
+    children: [
+      {
+        path: '/view-management',
+        name: 'ViewManagement',
+        component: ViewManagement
+      },
+      {
+        path: '/view-management/create-view',
+        name: 'CreateView',
+        component: CreateView
+      },
+      {
+        path: '/group-management',
+        name: 'GroupManagement',
+        component: GroupManagement
+      },
+      {
+        path: '/group-management/group-detail',
+        name: 'GroupDetail',
+        component: GroupDetail
+      },
+      {
+        path: '/app-management',
+        name: 'AppManagement',
+        component: AppManagement
+      },
+      {
+        path: '/app-management/create-app-management',
+        name: 'CreateAppManagement',
+        component: CreateAppManagement
+      },
+      {
+        path: '/tags-mnagement',
+        name: 'TagsManagement',
+        component: TagsManagement
+      }
+    ]
+  },
+]
+
