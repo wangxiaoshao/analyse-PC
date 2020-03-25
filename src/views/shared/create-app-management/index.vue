aa342e1324063bd1f915c5cc4cdfcedcf66c95b1
吴小坤 <657560745@qq.com>
2020-03-25 17:25:44 +0800

    调整创建应用界面的密码输入框

--- src/views/shared/create-app-management/index.vue
+++ src/views/shared/create-app-management/index.vue
@@ -46,7 +46,7 @@
         <el-col :span="12">
           <div class="grid-content bg-purple-light">
             <el-form-item label="接口账号密码" prop="apiPassword">
-              <el-input maxlength="28"  minlength="6" placeholder="请输入接口账号6-18位密码" v-model="appFrom.apiPassword" show-password auto-complete="false"></el-input>
+              <el-input maxlength="28"  minlength="6" placeholder="请输入接口账号6-18位密码" v-model="appFrom.apiPassword" show-password auto-complete="off"></el-input>
             </el-form-item>
           </div>
         </el-col>
