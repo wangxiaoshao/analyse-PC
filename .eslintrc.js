71ad9201301b250e0bfd1c49a7f3a0630d7f112b
吴小坤 <mormasmingo@gmail.com>
2020-05-15 16:54:29 +0800

    更新代码规范配置信息

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,33 +5,13 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
+    "plugin:prettier/recommended",
+    "prettier/standard",
   ],
   rules: {
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'linebreak-style': [
-      0,
-      'error',
-      'windows'
-    ],
-    'no-param-reassign': 0,
-    'no-shadow': 0,
-    'vue/max-attributes-per-line': 0,
-    'vue/html-self-closing': 0,
-    'max-len': 0,
-    'vue/html-closing-bracket-spacing': 0,
-    'vue/singleline-html-element-content-newline': 0,
-    'no-unused-expressions': 0,
-    'vue/attributes-order': 0,
-    'vue/mustache-interpolation-spacing': 0,
-    'eslint-disable-next-line': 1,
-    'no-multiple-empty-lines': [
-      1,
-      {
-        max: 2
-      }
-    ],
-    'eslint-disable-next-line': 0
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
   },
   parserOptions: {
     parser: 'babel-eslint'
