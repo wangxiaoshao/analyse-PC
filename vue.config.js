3cd592c2be46412a5683e8e7c69563f00c4d1f0c
吴小坤 <mormasmingo@gmail.com>
2020-05-20 15:45:35 +0800

    '引入打包文件管理，build之后自动生成zip包'

--- vue.config.js
+++ vue.config.js
@@ -1,51 +1,59 @@
-const path = require('path');
+const path = require("path");
 const GitRevisionPlugin = require("git-revision-webpack-plugin");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+    return path.join(__dirname, dir);
 }
 
 // 正是环境地址前缀，最好是绝对地址
-const publicPath = '';
+const publicPath = "";
 module.exports = function (options) {
-  options = options || {}
-  return {
-    publicPath: process.env.NODE_ENV === 'production' ? publicPath : '/',
+    options = options || {};
+    return {
+        publicPath: process.env.NODE_ENV === "production" ? publicPath : "/",
 
-    // 设置别名
-    chainWebpack: (config) => {
-      config.resolve.alias
-        .set('@src', resolve('src'))
-    },
-    productionSourceMap: false,
-    configureWebpack: config => {
-      if (process.env.NODE_ENV !== 'production') {
-        config.devtool = 'source-map'
-      }
-      let gitRevisionPlugin = new GitRevisionPlugin();
-      config.plugins.push(gitRevisionPlugin);
-    },
-
-    devServer: {
-      //publicPath: publicPath,
-      contentBase: ['dist', 'mock'],
-      disableHostCheck: true,
-      host: '0.0.0.0',
-      port: '8080',
-      proxy: {
-        "/api/": {
-          // target: "http://192.168.1.186",
-          // target: "http://jg-dev.lonmo.com/",
-            //  target: "http://10.226.9.210/",
-            // target: "http://59.215.232.95/",
-            target: "http://org.gz.cegn.cn/",
-             changeOrigin: false,
-          // http:"//jg-dev.lonmo.com",
-          // pathRewrite: {
-          //   "/api/": ""
-          // }
+        // 设置别名
+        chainWebpack: (config) => {
+            config.resolve.alias.set("@src", resolve("src"));
         },
-      }
-    }
-  }
-}
+        productionSourceMap: false,
+        configureWebpack: (config) => {
+            if (process.env.NODE_ENV !== "production") {
+                config.devtool = "source-map";
+            }
+
+            let gitRevisionPlugin = new GitRevisionPlugin();
+            let fileManagePlugin = new FileManagerPlugin({
+                onEnd: {
+                    delete: ["./dist.zip"],
+                    archive: [{ source: "./dist", destination: "./dist.zip" }],
+                }
+            });
+
+            config.plugins = [...config.plugins, gitRevisionPlugin, fileManagePlugin];
+        },
+
+        devServer: {
+            //publicPath: publicPath,
+            contentBase: ["dist", "mock"],
+            disableHostCheck: true,
+            host: "0.0.0.0",
+            port: "8080",
+            proxy: {
+                "/api/": {
+                    // target: "http://192.168.1.186",
+                    // target: "http://jg-dev.lonmo.com/",
+                    //  target: "http://10.226.9.210/",
+                    // target: "http://59.215.232.95/",
+                    target: "http://org.gz.cegn.cn/",
+                    changeOrigin: false,
+                    // http:"//jg-dev.lonmo.com",
+                    // pathRewrite: {
+                    //   "/api/": ""
+                    // }
+                },
+            },
+        },
+    };
+};
