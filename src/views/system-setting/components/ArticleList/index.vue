b4e65d3204a385b2111026d34d22270e890d2dd2
王小绍 <1936839949@qq.com>
2020-06-03 10:02:35 +0800

    文档中心优化

--- src/views/system-setting/components/ArticleList/index.vue
+++ src/views/system-setting/components/ArticleList/index.vue
@@ -65,7 +65,11 @@
                     下载附件
                     <i class="el-icon-download" style="color: #58a4f3;"></i>
                 </div>
-                <div class="chooseWord" style="text-align: center;">
+                <div
+                    class="chooseWord"
+                    v-if="accessoryList.length > 0"
+                    style="padding: 0 12px;"
+                >
                     <el-checkbox-group v-model="checkAccessoryList">
                         <el-checkbox
                             v-for="(item, index) in accessoryList"
@@ -74,6 +78,9 @@
                             >{{ item.name + item.suffix }}</el-checkbox
                         >
                     </el-checkbox-group>
+                </div>
+                <div class="chooseWord" style="text-align: center;" v-else>
+                    暂无可下载附件信息!
                 </div>
                 <div slot="footer" class="dialog-footer">
                     <el-button
@@ -162,8 +169,6 @@ export default {
             } else {
                 this.$message.error("请先选择要下载的附件");
             }
-
-            console.log(this.checkAccessoryList, 333333);
         },
         // 删除文档
         deleteWord(id) {
@@ -175,7 +180,6 @@ export default {
                 .then(() => {
                     api[urlNames["deleteDoc"]]({ id }).then((res) => {
                         if (res) {
-                            this.getGrid();
                             this.getGrid();
                             this.$message.success("操作成功");
                         }
@@ -189,6 +193,11 @@ export default {
 
     computed: {
         ...mapState(["app"]),
+    },
+    watch: {
+        activeName() {
+            this.getGrid();
+        },
     },
 };
 </script>
