5a1fd3eafbb6ebbad38518874645efb0171bb3de
王小绍 <1936839949@qq.com>
2020-07-19 17:23:08 +0800

    信息管理搜索全路径搜索

--- src/views/organization/components/Result/index.vue
+++ src/views/organization/components/Result/index.vue
@@ -4,25 +4,78 @@
             ref="popover"
             placement="bottom-start"
             v-if="resultFlag"
-            width="300"
+            width="500"
         >
             <div class="back-btn">
-                <el-button size="mini" @click="goBackTree">返回</el-button>
+                <el-button
+                    size="medium"
+                    type="text"
+                    icon="el-icon-close"
+                    @click="goBackTree"
+                ></el-button>
             </div>
             <div class="result-list">
+                <el-button
+                    size="medium"
+                    type="text"
+                    icon="el-icon-d-arrow-left"
+                    class="leftBtn btn"
+                    @click="pageReduce"
+                    :disabled="pageParams.page <= 1 ? true : false"
+                ></el-button>
+                <el-button
+                    size="medium"
+                    type="text"
+                    icon="el-icon-d-arrow-right"
+                    class="rightBtn btn"
+                    @click="pageAdd"
+                    :disabled="pageParams.page < allPages ? false : true"
+                ></el-button>
                 <el-table
                     v-loading="loadFlag"
                     :data="gridData"
                     :show-header="false"
+                    width="100%"
                 >
                     <el-table-column property="name">
                         <template slot-scope="scope">
-                            <span
-                                :title="scope.row.name"
-                                class="table-span"
-                                @click="setNodeId(scope.row)"
-                                >{{ scope.row.name }}</span
-                            >
+                            <div class="result-tr">
+                                <!-- <span
+                                    :title="scope.row.name"
+                                    class="table-span"
+                                     href
+                                     disabled
+                                    @click="setNodeId(scope.row)"
+                                    >{{ scope.row.name }}</span
+                                > -->
+                                <el-breadcrumb separator="/">
+                                    <el-breadcrumb-item
+                                        v-for="(item, index) in scope.row
+                                            .keyNav"
+                                        :key="index"
+                                    >
+                                        <span
+                                            class="table-span"
+                                            href
+                                            disabled
+                                            v-if="
+                                                scope.row.keyNav.length - 1 !==
+                                                index
+                                            "
+                                            >{{ item.name }}</span
+                                        >
+                                        <span
+                                            class="table-span navBar"
+                                            v-if="
+                                                scope.row.keyNav.length - 1 ===
+                                                index
+                                            "
+                                            @click.prevent="setNodeId(item.id)"
+                                            v-html="keyWordColor(item.name)"
+                                        ></span>
+                                    </el-breadcrumb-item>
+                                </el-breadcrumb>
+                            </div>
                         </template>
                     </el-table-column>
                 </el-table>
@@ -52,9 +105,15 @@
                         value="3"
                         title="内设机构"
                     ></el-option>
-                    <!--          <el-option label="节点" value="1"></el-option>-->
+                    <!--          <el-option label="节点"
+
+value="1"></el-option>-->
                 </el-select>
-                <el-button slot="append" icon="el-icon-search"></el-button>
+                <el-button
+                    slot="append"
+                    icon="el-icon-search"
+                    @click="getResult"
+                ></el-button>
             </el-input>
         </el-row>
     </div>
@@ -69,6 +128,7 @@ export default {
         return {
             debouncedSearch: null,
             resultFlag: false,
+            text: "",
             loadFlag: false,
             keyWord: "",
             value: "2",
@@ -76,6 +136,12 @@ export default {
             type: 2,
             restaurants: [],
             timer: null,
+            pageParams: {
+                page: 1,
+                limit: 5,
+                total: 0,
+            },
+            allPages: 0,
         };
     },
     props: ["defaultNodeId"],
@@ -83,11 +149,14 @@ export default {
         getType(el) {
             this.type = el;
         },
+
         // 获取搜索结果
         getResult() {
             let data = {
                 name: this.keyWord,
                 nodeType: this.type,
+                page: this.pageParams.page,
+                limit: this.pageParams.limit,
             };
             if (this.keyWord.length > 1) {
                 if (this.timer) {
@@ -95,14 +164,34 @@ export default {
                     this.timer = null;
                 }
                 // this.timer = this.debounce(this.getResultList, 800)
-                this.timer = setTimeout(() => {
-                    this.resultFlag = true;
-                    this.loadFlag = true;
-                    api[urlNames["searchViewNode"]](data).then((res) => {
-                        this.gridData = res.data;
-                        this.loadFlag = false;
-                    });
-                }, 800);
+                if (this.keyWord.length > 1) {
+                    this.timer = setTimeout(() => {
+                        this.resultFlag = true;
+                        this.loadFlag = true;
+                        api[urlNames["searchViewNode"]](data)
+                            .then((res) => {
+                                this.pageParams.total = res.data.total;
+                                this.allPages = Math.ceil(
+                                    this.pageParams.total /
+                                        this.pageParams.limit
+                                );
+                                let retData = [];
+                                if (res.data.list.length > 0) {
+                                    retData = this.convertData(
+                                        res.data.list[0]
+                                    );
+                                } else {
+                                    retData = [];
+                                }
+                                this.gridData = retData;
+                                this.loadFlag = false;
+                            })
+                            .catch(() => {
+                                this.gridData = [];
+                                this.loadFlag = false;
+                            });
+                    }, 500);
+                }
             } else {
                 this.resultFlag = false;
                 this.timer = null;
@@ -111,22 +200,72 @@ export default {
         blur() {
             this.timer = null;
         },
-        setNodeId(row) {
+        setNodeId(id) {
             this.$router.push({
                 name: "OrganizationContent",
                 params: {
-                    nodeId: row.id,
+                    nodeId: id,
                 },
             });
         },
         goBackTree() {
             this.keyWord = "";
             this.resultFlag = false;
-            this.$router.push({
-                name: "OrganizationContent",
-                params: {
-                    nodeId: this.defaultNodeId,
-                },
+            // this.$router.push({
+            //     name: "OrganizationContent",
+            //     params: {
+            //         nodeId: this.defaultNodeId,
+            //     },
+            // });
+        },
+        convertData(treeData) {
+            const keyNav = [];
+            const list = [];
+            this.getConvertData(treeData, keyNav, list);
+            return list;
+        },
+        getConvertData(treeData, keyNav, list) {
+            if (treeData.name) {
+                keyNav.push({
+                    name: treeData.name,
+                    id: treeData.id,
+                });
+            }
+            const children = treeData.children;
+            if (children && children.length) {
+                children.forEach((item) => {
+                    this.getConvertData(
+                        item,
+                        JSON.parse(JSON.stringify(keyNav)),
+                        list
+                    );
+                });
+            } else {
+                list.push({
+                    keyNav: keyNav,
+                });
+            }
+        },
+        pageReduce() {
+            this.pageParams.page--;
+            console.log(this.pageParams.page);
+            if (this.pageParams.page < 1) {
+                this.pageParams.page = 1;
+            }
+            this.getResult();
+        },
+        pageAdd() {
+            this.pageParams.page++;
+            console.log(this.pageParams.page);
+            this.getResult();
+        },
+        keyWordColor(name) {
+            return name.replace(this.keyWord, function (sMatch) {
+                // 为所匹配的单词添加颜色
+                return sMatch.replace(
+                    sMatch,
+                    '<span style="color:#58a4f3">' + sMatch + "</span>"
+                );
             });
         },
     },
@@ -134,6 +273,9 @@ export default {
         value(newVal) {
             this.type = newVal;
             this.getResult();
+        },
+        keyWord(newVal) {
+            this.pageParams.page = 1;
         },
     },
 };
